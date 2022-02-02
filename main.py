2fd88e94ae3ad8e504df2b166242dbe3a04330a9
azamat7087 <azaza7087@gmail.com>
2022-02-02 13:07:48 +0600

    L

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Query
-from schemas import Book
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author
+from typing import List
 
 app = FastAPI()
 
@@ -9,21 +10,21 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post('/book')
-def create_book(item: Book):
-    return item
-
 '''
  1. Три точки - обязательный параметр
- 2. min_l, max_l - длина параметра
+ 2. min_length=2, max_length=5 - длина параметра
  3. description - описание 
  4. Первый аргумент - значение по умолчанию
+ 5. List[str] - список параметров  с одним именем
+ 6. deprecated - Устаревший параметр
+ 7. gt - Наименьшее значение для int
+ 8. le - Наибольшее значение для int
+ 9. Path - для переменных в URL
+ 10. Query - для параметров
+ 11. Body - для добавления параметра в тело POST запроса
+ 12. response_model_exclude_unset=True - убирает ненужные данные с поля ответа
+ 13. response_model_exclude={"pages", "date"} - убирает указанные поля с ответа
 '''
-
-
-@app.get('/book')
-def get_book(parameter: str = Query("default", min_length=2, max_length=5, description="Parameter description")):
-    return parameter
 
 
 @app.get("/{name}")
@@ -32,3 +33,39 @@ async def say_hello(name: str, surname: str = None):
     if surname:
         response = {"message": f"Hello {name} {surname}"}
     return response
+
+
+""" List """
+
+
+@app.get('/books')
+def books_view(parameter: str = Query(...,
+                                      min_length=3,
+                                      max_length=10,
+                                      deprecated=True),
+               parameter_list: List[str] = Query(["default", "2"],
+                                                 description="Parameter description"),):
+    return parameter, parameter_list
+
+
+""" Retrieve """
+
+
+@app.get('/books/{pk}')
+def book_detail_view(pk: int = Path(..., gt=0, le=20),
+                     pages: int = Query(None, gt=0, le=500)):
+    return {"slug": pk,
+            "pages": pages}
+
+""" Create """
+
+
+@app.post('/books',)
+def create_book(item: Book = Body(..., embed=True)):
+
+    return {"item": item}
+
+
+@app.post('/author', response_model=Author)
+def create_author(author: Author = Body(..., embed=True)):
+    return {'author': author}
--- schemas.py
+++ schemas.py
@@ -1,18 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 from typing import List
 import datetime
 
 
 class Genre(BaseModel):
-    id: int
     name: str
 
 
+class Author(BaseModel):
+    name: str = Field(..., max_length=30, )
+    age: int = Field(..., gt=15, lt=90, description="Authors age")
+
+    # @validator('age')
+    # def validate_age(cls, value):
+    #     if value < 15:
+    #         raise ValueError('Author age must be more than 15')
+    #     elif value > 90:
+    #         raise ValueError('Author age must be less than 90')
+    #     return value
+
+
 class Book(BaseModel):
-    id: int
     title: str
     description: str
-    date: datetime.datetime  # Указание типа времени
+    date: datetime.datetime = Field(datetime.datetime.now(), )  # Указание типа времени
     # genres: str = None  # None по умолчанию
-    genres: List[Genre]  # Many to Many
+    genres: List[Genre] = []  # Many to Many
     pages: int = 20
